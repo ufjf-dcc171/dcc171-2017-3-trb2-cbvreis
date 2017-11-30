b80f02cf70a0a914f90a00e43737f96a1ee2c879
cassio reis <cassio_2000@hotmail.com>
2017-11-30 13:34:40 -0200

    Envio do segundo trabalho

---
+++ src/hamburgueria/Diretorio.java
@@ -0,0 +1,41 @@
+package hamburgueria;
+
+import java.io.File;
+import java.io.FileWriter;
+import javax.swing.JOptionPane;
+
+public class Diretorio {
+
+    private File arquivo = new File("funcionamento.txt");
+    private FileWriter arquivoFuncionamento;
+    private FileWriter arquivoItem;
+
+    public Diretorio() {
+        if (!arquivo.exists())
+            try {
+                arquivoFuncionamento = new FileWriter("funcionamento.txt");    
+                arquivoItem = new FileWriter("item.txt");    
+            } catch (Exception ex) {
+                JOptionPane.showMessageDialog(null, "Erro ao criar o diretorio");
+            }
+        }
+  
+    public FileWriter getArquivoFuncionamento() {
+        return arquivoFuncionamento;
+    }
+
+    public void setArquivoFuncionamento(FileWriter arquivoFuncionamento) {
+        this.arquivoFuncionamento = arquivoFuncionamento;
+    }
+    
+    public FileWriter getArquivoItem() {
+        return arquivoItem;
+    }
+
+    public void setArquivoItem(FileWriter arquivoItem) {
+        this.arquivoItem = arquivoItem;
+    }
+
+}
+
+    
---
+++ src/hamburgueria/JanelaRelatorio.java
@@ -0,0 +1,93 @@
+package hamburgueria;
+
+import java.awt.BorderLayout;
+import java.awt.Dimension;
+import java.awt.GridLayout;
+import java.awt.HeadlessException;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.Scanner;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.JScrollPane;
+import javax.swing.JTextArea;
+
+class JanelaRelatorio extends JFrame {
+
+    private Controlador janelaMesa;
+    private String linha;
+    private StringBuilder bg;
+    private JButton btnConfirma = new JButton("Voltar ao menu anterior");
+    private JButton btnRelatorio = new JButton("Gerar Relatorio");
+    private JPanel pnRelatorio = new JPanel();
+
+    public JanelaRelatorio(Controlador j) throws HeadlessException {
+        super("Relatório");
+        setMinimumSize(new Dimension(1200, 600));
+        setDefaultCloseOperation(JFrame.HIDE_ON_CLOSE);
+        setLocationRelativeTo(null);
+        this.janelaMesa = j;
+        JTextArea txtRelatorio = new JTextArea("");
+
+        //pnRelatorio.add();
+        add(new JScrollPane(txtRelatorio), BorderLayout.CENTER);
+
+        JPanel pnBotoes = new JPanel(new GridLayout(1, 1));
+        pnBotoes.add(btnRelatorio);
+        pnBotoes.add(btnConfirma);
+        add(pnBotoes, BorderLayout.SOUTH);
+        /*ESCREVE RELATORIO*/
+        btnRelatorio.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent ae) {
+
+                FileReader arq = null;
+                try {
+                    arq = new FileReader("funcionamento.txt");
+                    BufferedReader br = new BufferedReader(arq);
+                    StringBuilder sb = new StringBuilder();
+
+                    try {
+                        while ((linha = br.readLine()) != null) {
+                            sb.append(linha).append("\n");
+
+                        }
+
+                    } catch (IOException ex) {
+                        Logger.getLogger(JanelaRelatorio.class.getName()).log(Level.SEVERE, null, ex);
+                    }
+
+                    txtRelatorio.setText(sb.toString());
+                } catch (FileNotFoundException ex) {
+                    Logger.getLogger(JanelaRelatorio.class.getName()).log(Level.SEVERE, null, ex);
+                } finally {
+                    try {
+                        arq.close();
+                    } catch (IOException ex) {
+                        Logger.getLogger(JanelaRelatorio.class.getName()).log(Level.SEVERE, null, ex);
+                    }
+                }
+            }
+
+        });
+
+        btnConfirma.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent ae) {
+                janelaMesa.setVisible(true);
+                dispose();
+
+            }
+
+        });
+
+    }
+
+}
